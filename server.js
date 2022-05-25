792fd16eaca23ac13ca36050173aae373f7cadcb
GMerencio <gmerencio.santos@gmail.com>
2022-05-25 03:02:37 -0300

    Redirecionar requisições mal-sucedidas para o front end

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
+// Importações
 const path = require('path');
 const express = require('express');
 
+// Importações (arquivos locais)
 const iesRoute = require("./api/ies");
- 
-const app = express();
- 
-app.use(express.static(path.join(__dirname, 'build')));
-app.set('port', process.env.PORT || 5000);
 
+// Inicialização
+const app = express();
+
+// Especifica o caminho dos arquivos estáticos do front end
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Configurações de porta e parsing
+app.set('port', process.env.PORT || 5000);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
+// Rotas da aplicação
 app.use("/api/ies", iesRoute);
- 
+
+/* Handler para quando as rotas acima não suportarem uma requisição;
+redirecionar para o front end. */
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Escutar a porta especificada para requisições
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
