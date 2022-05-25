8bf71704435a67e5438d476d1f9d0cee50e2483c
GMerencio <gmerencio.santos@gmail.com>
2022-05-25 06:15:10 -0300

    Códigos adicionados aos filtros para facilitar consultas no BD

--- search/index.js
+++ search/index.js
@@ -20,30 +20,35 @@ function getFilters() {
 		lat: -3.10719,
 		long: -60.0261,
 		qtd_ies: 0,
+		CO_REGIAO_IES: '1',
 		estados: {}
 	};
 	filters['Nordeste'] = {
 		lat: -5.08921,
 		long: -42.8016,
 		qtd_ies: 0,
+		CO_REGIAO_IES: '2',
 		estados: {}
 	};
 	filters['Centro-Oeste'] = {
 		lat: -15.5989,
 		long: -56.0949,
 		qtd_ies: 0,
+		CO_REGIAO_IES: '5',
 		estados: {}
 	};
 	filters['Sul'] = {
 		lat: -27.5969,
 		long: -48.5495,
 		qtd_ies: 0,
+		CO_REGIAO_IES: '4',
 		estados: {}
 	};
 	filters['Sudeste'] = {
 		lat: -19.912998,
 		long: -43.940933,
 		qtd_ies: 0,
+		CO_REGIAO_IES: '3',
 		estados: {}
 	};
 	
@@ -70,21 +75,18 @@ async function run(){
         filters = getFilters();
         
         for (const regiao in filters) {
+        	const query_regiao = { NO_REGIAO_IES: regiao };
         	// Obter a quantidade de IES na região
-        	filters[regiao]['qtd_ies'] = await censo2020.countDocuments({
-        		NO_REGIAO_IES: regiao
-        	});
+        	filters[regiao]['qtd_ies'] = await censo2020.countDocuments(query_regiao);
         	
         	// Obter os estados da região
-        	const nomes_estados = await censo2020.distinct('NO_UF_IES', {
-        		NO_REGIAO_IES: regiao
-        	});
+        	const nomes_estados = await censo2020.distinct('NO_UF_IES', query_regiao);
         	
         	// Popular a região com os estados
         	for (const nome_estado of nomes_estados) {
         		const query = { NO_UF_IES: nome_estado };
         		let estado = await censo2020.findOne(query, {
-        			projection: {_id: 0, lat: 1, long: 1}
+        			projection: {_id: 0, lat: 1, long: 1, CO_UF_IES: 1}
         		});
         		estado['qtd_ies'] = await censo2020.countDocuments(query);
         		estado['municipios'] = {};
@@ -98,7 +100,7 @@ async function run(){
         		for (const nome_municipio of nomes_municipios) {
         			const query_municipio = { NO_MUNICIPIO_IES: nome_municipio };
         			let municipio = await censo2020.findOne(query_municipio, {
-        				projection: {_id: 0, lat: 1, long: 1}
+        				projection: {_id: 0, lat: 1, long: 1, CO_MUNICIPIO_IES: 1}
         			});
         			municipio['qtd_ies'] = await censo2020.countDocuments(query_municipio);
         			
