ae6da64f4ffce118f2afec607e5e2302222ee9f9
GMerencio <gmerencio.santos@gmail.com>
2022-06-15 06:35:51 -0300

    Página da instituição refeita com layout responsivo

--- src/components/PaginaIes/index.js
+++ src/components/PaginaIes/index.js
@@ -5,6 +5,31 @@ import { useParams, useNavigate } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import "./style.css";
+
+const ORG_ACADEMICA = {
+	'1': 'Universidade',
+	'2': 'Centro Universitário',
+	'3': 'Faculdade',
+	'4': 'Instituto Federal de Educação, Ciência e Tecnologia',
+	'5': 'Centro Federal de Educação Tecnológica'
+};
+
+const CATEGORIA_ADMIN = {
+	'1': 'Pública Federal',
+	'2': 'Pública Estadual',
+	'3': 'Pública Municipal',
+	'4': 'Privada com fins lucrativos',
+	'5': 'Privada sem fins lucrativos',
+	'6': 'Privada - Particular em sentido estrito',
+	'7': 'Especial',
+	'8': 'Privada comunitária',
+	'9': 'Privada confessional'
+
+};
 
 class PaginaIes extends React.Component {
   constructor(props) {
@@ -23,12 +48,16 @@ class PaginaIes extends React.Component {
   }
   
   render() {
-    if(!this.state.ies) {
+  	const ies = this.state.ies;
+    if(!ies) {
       return (<div />);
     }
     
+    const tipoOrg = ORG_ACADEMICA[ies["TP_ORGANIZACAO_ACADEMICA"]];
+    const catAdmin = CATEGORIA_ADMIN[ies["TP_CATEGORIA_ADMINISTRATIVA"]]
     return (
     	<div>
+    		{/* Header */}
     		<Box
     		 tabIndex="0"
     		 direction="column"
@@ -40,70 +69,147 @@ class PaginaIes extends React.Component {
     	  	 }}
     		>
     			<Chip
-  					label="Voltar"
-  					component="a"
-  					variant="outlined"
-  					clickable
-  					sx={{
-  						backgroundColor: '#fff',
-  						'&:hover': {
-  							backgroundColor: '#ebebeb !important',
-  						}
-  					}}
-  					onClick={() => {
-                    	this.props.navigate(-1);
-                  	}}
+    			 icon={<ArrowBackIcon />}
+  				 label="Retornar"
+  				 component="a"
+  				 variant="outlined"
+  				 clickable
+  				 sx={{
+  					backgroundColor: '#fff',
+  					fontWeight: '500',
+  					fontSize: '1.5em',
+  					'&:hover': {
+  						backgroundColor: '#ebebeb !important',
+  					}
+  				 }}
+  				 onClick={() => {
+                    this.props.navigate(-1);
+                 }}
 				/>
     			<Typography
-    				variant="h1"
-    				tabIndex="0"
-    				align="center">
-    				{this.state.ies["NO_IES"]}
+    			 variant="h1"
+    			 tabIndex="0"
+    			 align="center"
+    			 sx={{
+  					fontSize: '4rem'
+  				 }}
+    			>
+    				{`${ies["NO_IES"]} (${ies["SG_IES"]})`}
     			</Typography>
     		</Box>
     		
-        	<p tabIndex="0">
-        		Endereço: {this.state.ies["end_completo_y"]}
-        	</p>
-        	<p tabIndex="0">
-        		Quantidade de livros eletrônicos:{" "}
-        		{this.state.ies["QT_LIVRO_ELETRONICO"]}
-        	</p>
-        	<p tabIndex="0">
-        		Quantidade de periódicos eletrônicos:{" "}
-        		{this.state.ies["QT_PERIODICO_ELETRONICO"]}
-        	</p>
-        	<p tabIndex="0">
-        		Tem acesso ao portal Capes de periódicos?{" "}
-        		{this.state.ies["IN_ACESSO_PORTAL_CAPES"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Tem acesso a outras bases de dados licenciadas ou compradas?{" "}
-        		{this.state.ies["IN_ACESSO_OUTRAS_BASES"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Assina outras bases de dados licenciadas ou compradas?{" "}
-        		{this.state.ies["IN_ASSINA_OUTRA_BASE"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Tem catálogo online?{" "}
-        		{this.state.ies["IN_CATALOGO_ONLINE"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Oferece serviços pela internet?{" "}
-        		{this.state.ies["IN_SERVICO_INTERNET"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Busca integrada?{" "}
-        		{this.state.ies["IN_BUSCA_INTEGRADA"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		Biblioteca participa de redes sociais?{" "}
-        		{this.state.ies["IN_PARTICIPA_REDE_SOCIAL"] === "0" ? "Não" : "Sim"}
-        	</p>
-        	<p tabIndex="0">
-        		(Informações do censo de {this.state.ies["NU_ANO_CENSO"]})
-        	</p>
+    		{/* Grid do conteúdo */}
+    		<Grid
+    		 container
+    		 columnSpacing={2}
+    		 rowSpacing={1}
+    		>
+    			{/* Informações da biblioteca */}
+    			<Grid item xs={12} md={6}>
+    			  <div className="section-container">
+    			  	<Typography
+    			  	 variant="h2"
+    			  	 className="section-header">
+    			  		Informações da biblioteca
+    			  	</Typography>
+    			  	<Typography>
+                    	Quantidade de livros eletrônicos:{" "}
+                    	{ies["QT_LIVRO_ELETRONICO"]}
+                  	</Typography>
+                  	<Typography>
+                    	Quantidade de periódicos eletrônicos:{" "}
+                    	{ies["QT_PERIODICO_ELETRONICO"]}
+                  	</Typography>
+                  	<Typography>
+                    	Tem acesso ao portal Capes de periódicos?{" "}
+                    	{ies["IN_ACESSO_PORTAL_CAPES"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Tem acesso a outras bases de dados licenciadas ou compradas?{" "}
+                    	{ies["IN_ACESSO_OUTRAS_BASES"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Assina outras bases de dados licenciadas ou compradas?{" "}
+                    	{ies["IN_ASSINA_OUTRA_BASE"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Tem catálogo online?{" "}
+                    	{ies["IN_CATALOGO_ONLINE"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Oferece serviços pela internet?{" "}
+                    	{ies["IN_SERVICO_INTERNET"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Busca integrada?{" "}
+                    	{ies["IN_BUSCA_INTEGRADA"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  	<Typography>
+                    	Biblioteca participa de redes sociais?{" "}
+                    	{ies["IN_PARTICIPA_REDE_SOCIAL"] === "0" ? "Não" : "Sim"}
+                  	</Typography>
+                  </div>
+    			</Grid>
+    			
+    			<Grid
+    			 container
+    			 item
+    			 xs={12}
+    			 md={6}
+    			 rowSpacing={2}
+    			>
+    				{/* Informações administrativas */}
+    				<Grid item xs={12} md={12}>
+    					<div className="section-container">
+    						<Typography
+    			  	 		 variant="h2"
+    			  	 		 className="section-header">
+    			  				Informações da instituição
+    			  			</Typography>
+    						<Typography>Ano do Censo: {ies["NU_ANO_CENSO"]}</Typography>
+    						<Typography>Código da instituição: {ies["CO_IES"]}</Typography>
+    						<Typography>Mantenedora: {ies["NO_MANTENEDORA"]}</Typography>
+    						<Typography>Tipo da Organização Acadêmica: {tipoOrg}</Typography>
+    						<Typography>Tipo da Categoria Administrativa: {catAdmin}</Typography>
+    					</div>
+    				</Grid>
+    				
+    				{/* Informações geográficas */}
+    				<Grid item xs={12} md={12}>
+    					<div className="section-container">
+    						<Typography
+    			  	 		 variant="h2"
+    			  	 		 className="section-header">
+    			  				Informações geográficas
+    			  			</Typography>
+    						<Typography>Região: {ies["NO_REGIAO_IES"]}</Typography>
+    						<Typography>Mesorregião: {ies["NO_MESORREGIAO_IES"]}</Typography>
+    						<Typography>Microrregião: {ies["NO_MICRORREGIAO_IES"]}</Typography>
+    						<Typography>Estado: {ies["NO_UF_IES"]}</Typography>
+    						<Typography>Município: {ies["NO_MUNICIPIO_IES"]}</Typography>
+    						<Typography>Endereço: {ies["end_completo_y"]}</Typography>
+    					
+    						<div class="mapouter">
+  								<div class="gmap_canvas">
+    								<iframe
+    								title="mapa"
+    							 	width="600"
+    							 	height="500"
+    							 	id="mapaIframe"
+    							 	src={`https://maps.google.com/maps?q=${ies['end_completo_y']}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+    							 	frameborder="0"
+    							 	scrolling="no"
+    							 	marginheight="0"
+    							 	marginwidth="0"
+    								>
+    								</iframe>
+  								</div>
+							</div>
+
+    					</div>
+    				</Grid>
+    			</Grid>
+    		</Grid>
         </div>
     );
   }
